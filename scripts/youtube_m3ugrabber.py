d00a26949d70fffc2e55a270864555ded36dbb95
kaka111222333 <102349696+kaka111222333@users.noreply.github.com>
2024-02-01 22:17:07 +0800

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -1,11 +1,10 @@
 #! /usr/bin/python3
 
 banner = r'''
-#########################################################################
+CCTV-1 综合,http://[2409:8087:2001:20:2800:0:df6e:eb02]/ott.mobaibox.com/PLTV/4/224/3221228149/index.m3u8
 凤凰资讯,http://[2409:8087:2001:20:2800:0:df6e:eb18]:80/ott.mobaibox.com/PLTV/3/224/3221228524/index.m3u8
 凤凰卫视,http://[2409:8087:2001:20:2800:0:df6e:eb23]:80/ott.mobaibox.com/PLTV/3/224/3221228527/index.m3u8
-CCTV-1 综合,http://[2409:8087:2001:20:2800:0:df6e:eb02]/ott.mobaibox.com/PLTV/4/224/3221228149/index.m3u8
-#########################################################################
+
 '''
 
 import requests
@@ -13,40 +12,40 @@ import os
 import sys
 
 windows = False
-if 'win' in sys.platform:
+if 'win' 在 sys.platform:
     windows = True
 
 def grab(url):
     response = requests.get(url, timeout=15).text
-    if '.m3u8' not in response:
+    if '.m3u8' not 在 response:
         #response = requests.get(url).text
-        if '.m3u8' not in response:
+        if '.m3u8' not 在 response:
             if windows:
                 print('https://0.m3u')
                 return
             #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
-            if '.m3u8' not in response:
+            if '.m3u8' not 在 response:
                 print('https://0.m3u')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
     while True:
-        if 'https://' in response[end-tuner : end]:
+        if 'https://' 在 response[end-tuner : end]:
             link = response[end-tuner : end]
             start = link.find('https://')
             end = link.find('.m3u8') + 5
             break
         else:
             tuner += 5
-    print(f"{link[start : end]}")
+    print(f",{link[start : end]}")
 
 print('#EXTM3U x-tvg-url="https://github.com/botallen/epg/releases/download/latest/epg.xml"')
 print(banner)
 #s = requests.Session()
 with open('../youtube_channel_info.txt') as f:
-    for line in f:
+    for line 在 f:
         line = line.strip()
         if not line or line.startswith('~~'):
             continue
@@ -56,7 +55,7 @@ with open('../youtube_channel_info.txt') as f:
             grp_title = line[1].strip().title()
             tvg_logo = line[2].strip()
             tvg_id = line[3].strip()
-            print(f'\n {ch_name},',)
+            print(f'{ch_name}',)
         else:
             grab(line)
             
