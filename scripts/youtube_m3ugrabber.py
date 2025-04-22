8bcc08d2e5fc6869202a0889823c94f18ddf5fa2
kaka111222333 <102349696+kaka111222333@users.noreply.github.com>
2025-04-22 14:27:23 +0800

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -47,13 +47,13 @@ def grab(url):
         #response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('000000')
+                print('0')
                 return
             #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('000000')
+                print('00')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
